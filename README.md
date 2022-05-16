2510d432e80305501ada01d5b3eb9cce7e90445d
Tomio <mail@tomio.fun>
2022-05-16 17:09:08 +0800

    feat: init project

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # procfile
-Procfile parser for Rust
+
+[![Crate Version](https://img.shields.io/crates/v/procfile)](https://crates.io/crates/procfile)
+[![Documentation](https://docs.rs/procfile/badge.svg)](https://docs.rs/procfile)
+[![License](https://img.shields.io/crates/l/procfile.svg)](./LICENSE-APACHE)
+[![Continuous Delivery](https://github.com/devtomio/procfile/actions/workflows/continuous-delivery.yml/badge.svg)](https://github.com/devtomio/procfile/actions/workflows/continuous-delivery.yml)
+[![Continuous Integration](https://github.com/devtomio/procfile/actions/workflows/continuous-integration.yml/badge.svg)](https://github.com/devtomio/procfile/actions/workflows/continuous-integration.yml)
+
+**Procfile parser for Rust**
+
+## Example Usage
+
+> Cargo.toml
+
+```toml
+[dependencies]
+procfile = "0.1"
+```
+
+The code:
+
+```rs
+use procfile;
+
+fn main() {
+    let procfile = "web: node hello-world.js --verbose";
+    let parsed = procfile::parse(procfile).expect("Failed parsing procfile");
+    let web = parsed.get("web").expect("Failed getting web process");
+
+    println!("{:?}", web);
+}
+```
+
+## Cargo Features
+
+- `rayon` - Enables [rayon](https://github.com/rayon-rs/rayon) support (enabled by default)
+- `serde` - Enables [serde](https://serde.rs) support
+
+## Contributors ✨
+
+Thanks goes to these wonderful people ([emoji key](https://allcontributors.org/docs/en/emoji-key)):
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<table>
+  <tr>
+    <td align="center"><a href="https://tomio.fun/"><img src="https://avatars.githubusercontent.com/u/75403863?v=4?s=100" width="100px;" alt=""/><br /><sub><b>Tomio</b></sub></a><br /><a href="https://github.com/devtomio/procfile/commits?author=devtomio" title="Code">💻</a> <a href="https://github.com/devtomio/procfile/commits?author=devtomio" title="Documentation">📖</a> <a href="#example-devtomio" title="Examples">💡</a> <a href="#infra-devtomio" title="Infrastructure (Hosting, Build-Tools, etc)">🚇</a> <a href="#maintenance-devtomio" title="Maintenance">🚧</a></td>
+  </tr>
+</table>
+
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+This project follows the [all-contributors](https://github.com/all-contributors/all-contributors) specification. Contributions of any kind welcome!
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or http://apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
---
+++ src/lib.rs
@@ -0,0 +1,174 @@
+//! # procfile
+//! 
+//! A rust library for parsing Procfile(s).
+//! 
+//! ## Examples
+//! 
+//! ```rs
+//! let my_procfile = "web: cargo run";
+//! let parsed = procfile::parse(my_procfile).expect("Failed parsing procfile");
+//! let web_process = parsed.get("web").expect("Failed getting web process");
+//! 
+//! assert_eq!("cargo", web_process.command);
+//! assert_eq!(vec!["run"], web_process.options);
+//! ```
+
+use cfg_if::cfg_if;
+use dashmap::DashMap;
+use lazy_static::lazy_static;
+#[cfg(feature = "rayon")]
+use rayon::prelude::*;
+use regex::Regex;
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+/// Parses a Procfile string.
+/// 
+/// # Examples
+/// 
+/// ```rs
+/// use procfile;
+/// 
+/// let my_procfile = "web: cargo run";
+/// let parsed = procfile::parse(my_procfile).expect("Failed parsing procfile");
+/// let web_process = parsed.get("web").expect("Failed getting web process");
+/// 
+/// assert_eq!("cargo", web_process.command);
+/// assert_eq!(vec!["run"], web_process.options);
+/// ```
+///
+/// # Errors
+/// 
+/// - When building the regex fails
+/// - When either the command, options, and the process name don't exist but the regex matched
+pub fn parse<'a>(content: &'a str) -> Result<DashMap<&'a str, Process>> {
+    lazy_static! {
+        static ref REGEX: Regex =
+            Regex::new(r"^([A-Za-z0-9_]+):\s*(.+)$").expect("Failed building regex");
+    }
+
+    let map: DashMap<&'a str, Process> = DashMap::new();
+
+    #[cfg(feature = "rayon")]
+    content.split('\n').par_bridge().for_each(|line| match REGEX.captures(line) {
+        Some(captures) => {
+            let details = captures
+                .get(2)
+                .expect("Failed getting command and options")
+                .as_str()
+                .trim()
+                .split(' ')
+                .collect::<Vec<_>>();
+
+            let name = captures.get(1).expect("Failed getting process name").as_str();
+
+            map.insert(name, Process {
+                command: details[0],
+                options: details[1..].to_vec(),
+            });
+        },
+        None => (),
+    });
+
+    #[cfg(not(feature = "rayon"))]
+    content.split('\n').for_each(|line| match REGEX.captures(line) {
+        Some(captures) => {
+            let details = captures
+                .get(2)
+                .expect("Failed getting command and options")
+                .as_str()
+                .trim()
+                .split(' ')
+                .collect::<Vec<_>>();
+
+            let name = captures.get(1).expect("Failed getting process name").as_str();
+
+            map.insert(name, Process {
+                command: details[0],
+                options: details[1..].to_vec(),
+            });
+        },
+        None => (),
+    });
+
+    Ok(map)
+}
+
+cfg_if! {
+    if #[cfg(feature = "serde")] {
+        use serde::{Serialize, Deserialize};
+
+        /// Represents a single process.
+        #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+        pub struct Process<'a> {
+            /// The command to use. (e.g. `cargo`)
+            pub command: &'a str,
+            /// The command options. (e.g. `["build", "--release"]`)
+            pub options: Vec<&'a str>,
+        }
+    } else {
+        /// Represents a single process.
+        #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+        pub struct Process<'a> {
+            /// The command to use. (e.g. `cargo`)
+            pub command: &'a str,
+            /// The command options. (e.g. `["build", "--release"]`)
+            pub options: Vec<&'a str>,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_one_process() {
+        let procfile = "web: node a.js --option-1 --option-2";
+        let parsed = parse(procfile).unwrap();
+
+        assert!(parsed.contains_key("web"));
+
+        let process = parsed.get("web").unwrap();
+
+        assert_eq!("node", process.command);
+        assert_eq!(vec!["a.js", "--option-1", "--option-2"], process.options)
+    }
+
+    #[test]
+    fn test_multiple_process() {
+        let procfile = "\
+web: py b.py --my-option
+worker: gcc c.c    
+        ";
+
+        let parsed = parse(procfile).unwrap();
+
+        assert!(parsed.contains_key("web") && parsed.contains_key("worker"));
+
+        let web = parsed.get("web").unwrap();
+        let worker = parsed.get("worker").unwrap();
+
+        assert_eq!("py", web.command);
+        assert_eq!("gcc", worker.command);
+        assert_eq!(vec!["b.py", "--my-option"], web.options);
+        assert_eq!(vec!["c.c"], worker.options);
+    }
+
+    #[test]
+    fn test_no_process() {
+        let procfile = "";
+        let parsed = parse(procfile).unwrap();
+
+        assert!(parsed.is_empty());
+    }
+
+    #[test]
+    fn test_invalid_process() {
+        let procfile = "hedhehiidhodhidhiodiedhidwhio";
+        let parsed = parse(procfile).unwrap();
+
+        assert!(parsed.is_empty());
+    }
+}
